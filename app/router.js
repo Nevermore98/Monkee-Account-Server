d9c46637156824fa493c8ed7acc546dfcb38a9a8
Nevermore98 <Nevermorey98@qq.com>
2022-02-24 00:01:42 +0800

    实现添加账单接口

--- app/router.js
+++ app/router.js
@@ -6,10 +6,18 @@
 module.exports = (app) => {
   const { router, controller, middleware } = app
   const _jwt = middleware.jwtErr(app.config.jwt.secret) // 传入加密字符串
+  // 注册
   router.post('/api/user/register', controller.user.register)
+  // 登录
   router.post('/api/user/login', controller.user.login)
-  router.get('/api/user/userInfo', _jwt, controller.user.getUserInfo) // 获取用户信息
-  router.patch('/api/user/userInfo', _jwt, controller.user.editUserInfo) // 修改用户个性签名
+  // 获取用户信息
+  router.get('/api/user/userInfo', _jwt, controller.user.getUserInfo)
+  // 修改用户信息（目前仅能修改签名）
+  router.patch('/api/user/userInfo', _jwt, controller.user.editUserInfo)
+  // 上传头像
   router.post('/api/uploadAvatar', controller.upload.uploadAvatar)
+  // 添加账单
+  router.post('/api/bill/add', _jwt, controller.bill.add)
+  // 测试
   router.get('/api/user/test', _jwt, controller.user.test) // 放入第二个参数，作为中间件过滤项
 }
